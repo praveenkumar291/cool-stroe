576dafd4e9826daca2db371adc83408a9ad2d257
praveen291 <mudadlapraveenkumar143@gmail.com>
2022-01-10 19:09:35 +0530

    layoutStyling

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import Head from 'next/head'
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import useStyles from '../../utils/style';
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazona</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Cool-Store</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
-        <Typography>
-
-          All rights reserved.Cool-store
-        </Typography>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
+        <Typography>All rights reserved.Cool-store</Typography>
       </footer>
-
     </div>
   );
 };
